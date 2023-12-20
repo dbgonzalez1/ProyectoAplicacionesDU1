1ba6419a229053708dbaea2e0492d681724df1ea
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-12-19 23:18:04 -0500

    Archivo Controllers

---
+++ BACKEND/Controllers/TransmisionEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using backend.Dto;
+using backend.Models;
+using backend.Services;
+
+namespace backend.Controllers;
+
+public static class TransmisionEndpoints
+{
+    public static void MapTransmisionEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/Transmision");
+
+        group.MapGet("/", TransmisionService.GetAll)
+            .WithName("GetAllTransmisions")
+            .Produces<List<Transmision>>();
+
+        group.MapGet("/{id:int}", TransmisionService.Find)
+            .WithName("GetTransmisionById")
+            .Produces<Transmision>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        group.MapPut("/{id:int}", TransmisionService.Update)
+            .WithName("UpdateTransmision")
+            .Accepts<TransmisionDto>("application/json")
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent);
+
+        group.MapPost("/", TransmisionService.Create)
+            .WithName("CreateTransmision")
+            .Accepts<TransmisionDto>("application/json")
+            .Produces<Transmision>(StatusCodes.Status201Created);
+
+        group.MapDelete("/{id:int}", TransmisionService.Delete)
+            .WithName("DeleteTransmision")
+            .Produces(StatusCodes.Status404NotFound);
+    }
+}
