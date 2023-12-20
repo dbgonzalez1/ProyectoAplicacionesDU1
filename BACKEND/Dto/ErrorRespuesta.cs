9bdf60b9922d60ae766c84db36a766129e01de5e
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:06:43 -0500

    Archivo ErrorRespuesta
    ErrorRespuesta

---
+++ BACKEND/Dto/ErrorRespuesta.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace backend.Dto;
+
+public class ErrorRespuesta(string mensaje)
+{
+    public string message { get; set; } = mensaje;
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
