e002329e0fa6ae0e1569ee11f6eadd06bbaae92c
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:17:15 -0500

    Directorio Exceptions
    ErrorDeCliente

---
+++ BACKEND/Exceptions/ErrorDeCliente.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace backend.Exceptions;
+
+public class ErrorDeCliente(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest) : Exception(message)
+{
+     public HttpStatusCode StatusCode { get; } = statusCode;
+     
+}
