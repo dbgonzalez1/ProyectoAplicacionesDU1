91d25e4e0ae0d36d24744300857ad7fea23f5cee
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:19:10 -0500

    Auto
    Auto

---
+++ BACKEND/Models/Auto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Auto : HasEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50, ErrorMessage = "El nombre debe tener entre 2 y 50 caracteres", MinimumLength = 2)]
+        public string Modelo { get; set; }
+
+        public MarcaAuto? Marca { get; set; }
+        public Amortiguador? Amortiguador { get; set; }
+        public Transmision? Transmision { get; set; }
+        public Suspension? Suspension { get; set; }
+        public Motor? Motor { get; set; }
+        public Frenos? Frenos { get; set; }
+        public Neumatico? Neumatico { get; set; }
+    }
+}
