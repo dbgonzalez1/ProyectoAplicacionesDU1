2fd8993baedba696a98f0dec223b03574c43d91a
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:14:39 -0500

    SuspensionDto
    SuspensionDto

---
+++ BACKEND/Dto/SuspensionDto.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Dto;
+
+public class SuspensionDto
+{
+    public string Marca { get; set; }
+
+    public string Modelo { get; set; }
+
+    public float Rigidez { get; set; }
+
+    public float Altura { get; set; }
+
+    public float CapacidadDeCarga { get; set; }
+
+    public int IdAmortiguador { get; set; }
+
+    public int IdProveedor { get; set; }
+
+}
