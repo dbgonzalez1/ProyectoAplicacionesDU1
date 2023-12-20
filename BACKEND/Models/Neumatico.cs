c8789784d13d615dcbfdc651e6b5d1d3f14a7c06
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:20:43 -0500

    Neumatico
    Neumatico

---
+++ BACKEND/Models/Neumatico.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class Neumatico:HasEntity
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "La marca es requerida")]
+    [StringLength(50, ErrorMessage = "La marca debe tener entre 2 y 50 caracteres", MinimumLength = 2)]
+    public string Marca { get; set; }
+
+    [Required(ErrorMessage = "El modelo es requerido")]
+    [StringLength(50, ErrorMessage = "El modelo debe tener entre 2 y 50 caracteres", MinimumLength = 2)]
+    public string Modelo { get; set; }
+
+    [Required(ErrorMessage = "El precio es requerido")]
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public float Relacion { get; set; }
+
+    [Required(ErrorMessage = "El precio es requerido")]
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public float Eficiencia { get; set; }
+
+    [Required(ErrorMessage = "El precio es requerido")]
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public float Consumo { get; set; }
+
+    [Required(ErrorMessage = "El precio es requerido")]
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public float NumeroDeMarchas { get; set; }
+
+    public Proveedor? Proveedor { get; set; }
+}
