8b0bfdbe6d6ca938f4d3284f6b3bdb04fd16776e
jjlopez17 <124115187+jjlopez17@users.noreply.github.com>
2023-12-19 23:24:22 -0500

    Validador
    Se creo la clase Validador

---
+++ BACKEND/Services/Validador.cs
@@ -0,0 +1,15 @@
+﻿
+using backend.Exceptions;
+
+namespace backend.Services;
+
+public abstract class Validador
+{
+    public static void ValidarRelaciones(bool tieneRelaciones, string relacion)
+    {
+        if (tieneRelaciones)
+        {
+            throw new ErrorDeCliente($"No se puede eliminar la entidad porque tiene {relacion} asociados");
+        }
+    }
+}
