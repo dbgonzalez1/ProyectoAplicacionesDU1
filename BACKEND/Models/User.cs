d508e74e04fa43d321419c4e7bca4b6ac740a5a6
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:21:54 -0500

    User
    User

---
+++ BACKEND/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace backend.Models;
+
+public class User
+{
+    public string? Id { get;  set; }
+    public string? Username { get;  set; }
+    public string? Password { get;  set; }
+
+    public static readonly List<User> TestUsers =
+    [
+        new User
+        {
+            Id = "1",
+            Username = "admin",
+            Password = "admin"
+        },
+
+        new User
+        {
+            Id = "2",
+            Username = "user",
+            Password = "user"
+        }
+    ];
+}
