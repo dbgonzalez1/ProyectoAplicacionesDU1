1e99fa9b428b242406d43a08aeeac4be79acb556
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:13:37 -0500

    MotorDto
    MotorDto

---
+++ BACKEND/Dto/MotorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Dto;
+
+public class MotorDto
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public float Potencia { get; set; }
+    public float Torque { get; set; }
+    public float Eficiencia { get; set; }
+    public float Consumo { get; set; }
+    public float Cilindrada { get; set; }
+    public int IdProveedor { get; set; }
+}
