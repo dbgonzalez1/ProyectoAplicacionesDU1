8aa39363289ec98d04179b239bb2d73e93f83693
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-12-19 23:19:06 -0500

    Archivo Data

---
+++ BACKEND/Data/DataRepository.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models;
+
+namespace backend.Data;
+
+public static class DataRepository
+{
+    
+    public static RepositoryCollection<MarcaAuto> MarcaAuto { get; } = [];
+    public static RepositoryCollection<Auto> Auto { get; } = [];
+    public static RepositoryCollection<Proveedor> Proveedor { get; } = [];
+    public static RepositoryCollection<Amortiguador> Amortiguador { get; } = [];
+    public static RepositoryCollection<Frenos> Frenos { get; } = [];
+    public static RepositoryCollection<Motor> Motor { get; } =[];
+    public static RepositoryCollection<Neumatico> Neumatico { get; } = [];
+    public static RepositoryCollection<Suspension> Suspension { get; } = [];
+    public static RepositoryCollection<Transmision> Transmision { get; } = [];
+}
