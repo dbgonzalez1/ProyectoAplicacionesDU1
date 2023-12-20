92592adcdd7631bbeba27435ebfabc9a824d59d8
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:14:06 -0500

    NeumaticoDto
    NeumaticoDto

---
+++ BACKEND/Dto/NeumaticoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Dto;
+
+public class NeumaticoDto
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public float Relacion { get; set; }
+    public float Eficiencia { get; set; }
+    public float Consumo { get; set; }
+    public float NumeroDeMarchas { get; set; }
+    public int IdProveedor { get; set; }
+}
