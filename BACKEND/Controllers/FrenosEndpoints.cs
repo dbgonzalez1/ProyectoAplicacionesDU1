f696bcd30637afd0cc1a791988c5fae77258167d
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-12-19 23:16:07 -0500

    Archivo Controllers

---
+++ BACKEND/Controllers/FrenosEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using backend.Dto;
+using backend.Models;
+using backend.Services;
+
+namespace backend.Controllers;
+
+public static class FrenosEndpoints
+{
+    public static void MapFrenosEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/Frenos").RequireAuthorization();
+
+        group.MapGet("/", FrenosService.GetAll)
+            .WithName("GetAllFrenos")
+            .Produces<List<Frenos>>();
+
+        group.MapGet("/{id:int}", FrenosService.Find)
+            .WithName("GetFrenosById")
+            .Produces<Frenos>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        group.MapPut("/{id:int}", FrenosService.Update)
+            .WithName("UpdateFrenos")
+            .Accepts<FrenosDto>("application/json")
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent);
+
+        group.MapPost("/", FrenosService.Create)
+            .WithName("CreateFrenos")
+            .Accepts<FrenosDto>("application/json")
+            .Produces<Frenos>(StatusCodes.Status201Created);
+
+        group.MapDelete("/{id:int}", FrenosService.Delete)
+            .WithName("DeleteFrenos")
+            .Produces(StatusCodes.Status404NotFound);
+    }
+}
