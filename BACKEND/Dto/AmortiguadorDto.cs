104118fd7dd4ea0af05711cbab425fa11f44f4b0
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:04:57 -0500

    Directorio Dto
    AmortiguadorDto

---
+++ BACKEND/Dto/AmortiguadorDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace backend.Dto;
+
+public class AmortiguadorDto
+{
+    public string Marca { get; set; } = null!;
+    public string Modelo { get; set; } = null!;
+    public float Recorrido { get; set; }
+    public float Fuerza { get; set; }
+    public int AñosUtil { get; set; }
+    public float DiametroDelPiston { get; set; }
+    public int IdProveedor { get; set; }
+}
