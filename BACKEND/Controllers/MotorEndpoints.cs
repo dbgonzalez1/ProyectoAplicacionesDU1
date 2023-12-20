3c80e346ad5ced92826046f65583fe421a5e7166
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-12-19 23:16:39 -0500

    Archivo Controllers

---
+++ BACKEND/Controllers/MotorEndpoints.cs
@@ -0,0 +1,38 @@
+﻿using backend.Dto;
+using backend.Models;
+using backend.Services;
+
+namespace backend.Controllers;
+
+public static class MotorEndpoints
+{
+    public static void MapMotorEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/Motor").RequireAuthorization();
+
+        group.MapGet("/", MotorService.GetAll)
+            .WithName("GetAllMotors")
+            .Produces<List<Motor>>();
+
+        group.MapGet("/{id:int}", MotorService.Find)
+            .WithName("GetMotorById")
+            .Produces<Motor>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        group.MapPut("/{id:int}", MotorService.Update)
+            .WithName("UpdateMotor")
+            .Accepts<MotorDto>("application/json")
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent);
+
+        group.MapPost("/", MotorService.Create)
+            .WithName("CreateMotor")
+            .Accepts<MotorDto>("application/json")
+            .Produces<Motor>(StatusCodes.Status201Created);
+
+        group.MapDelete("/{id:int}", MotorService.Delete)
+            .WithName("DeleteMotor")
+            .Produces<Motor>()
+            .Produces(StatusCodes.Status404NotFound);
+    }
+}
