134cb2aaf164cef8a3c4648f74b25a4d0097db0c
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:07:03 -0500

    Archivo FrenosDto
    FrenosDto

---
+++ BACKEND/Dto/FrenosDto.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Dto;
+
+public class FrenosDto
+{
+
+    public string Tipo { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public double Precio { get; set; }
+    public int IdProveedor { get; set; }
+
+}
