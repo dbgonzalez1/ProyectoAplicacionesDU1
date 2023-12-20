69dcefda6255e7e612376d346e4bbf1fd1f28dcd
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:20:12 -0500

    MarcaAuto
    MarcaAuto

---
+++ BACKEND/Models/MarcaAuto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class MarcaAuto : HasEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50, ErrorMessage = "El nombre debe tener entre 2 y 50 caracteres", MinimumLength = 2)]
+        public string Nombre { get; set; }
+    }
+}
