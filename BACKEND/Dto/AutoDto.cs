b49d99bae5b6ff3bd64ec1f4cf2cd5d640b1a7a4
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:05:54 -0500

    Archivo AutoDto
    AutoDto

---
+++ BACKEND/Dto/AutoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Dto;
+
+public class AutoDto
+{
+    public string Modelo { get; set; } = null!;
+    public int IdMarca { get; set; }
+    public int IdAmortiguador { get; set; }
+    public int IdTransmision { get; set; }
+    public int IdSuspension { get; set; }
+    public int IdMotor { get; set; }
+    public int IdFrenos { get; set; }
+    public int IdNeumatico { get; set; }
+}
