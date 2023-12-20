f539d0e5d4e8bcf92c877ecc08a5886b38ec2a52
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-12-19 23:17:06 -0500

    Archivo Controllers

---
+++ BACKEND/Controllers/ProveedorEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using backend.Dto;
+using backend.Models;
+using backend.Services;
+
+namespace backend.Controllers;
+
+public static class ProveedorEndpoints
+{
+    public static void MapProveedorEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/Proveedor").RequireAuthorization();
+
+        group.MapGet("/", ProveedorService.GetAll)
+            .WithName("GetAllProveedors")
+            .Produces<List<Proveedor>>();
+
+        group.MapGet("/{id:int}", ProveedorService.Find)
+            .WithName("GetProveedorById")
+            .Produces<Proveedor>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        group.MapPut("/{id:int}", ProveedorService.Update)
+            .WithName("UpdateProveedor")
+            .Accepts<ProveedorDto>("application/json")
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent);
+
+        group.MapPost("/", ProveedorService.Create)
+            .WithName("CreateProveedor")
+            .Accepts<ProveedorDto>("application/json")
+            .Produces<Proveedor>(StatusCodes.Status201Created);
+
+        group.MapDelete("/{id:int}", ProveedorService.Delete)
+            .WithName("DeleteProveedor")
+            .Produces(StatusCodes.Status404NotFound);
+    }
+}
