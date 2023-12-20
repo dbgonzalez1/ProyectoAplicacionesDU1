5b9aeeb78c908dee735ad2305b97a5f52c7e5742
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:15:35 -0500

    TransmisionDto
    TransmisionDto

---
+++ BACKEND/Dto/TransmisionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Dto;
+
+public class TransmisionDto
+{
+    public string Tipo { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public float Relacion { get; set; }
+    public float Eficiencia { get; set; }
+    public float Consumo { get; set; }
+    public float NumeroDeMarchas { get; set; }
+    public int IdProveedor { get; set; }
+}
