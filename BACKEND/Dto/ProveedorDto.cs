845ac7a0c5869fc854a01791db8c9e4f2c3b5713
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-12-19 23:14:21 -0500

    ProveedorDto
    ProveedorDto

---
+++ BACKEND/Dto/ProveedorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Dto;
+
+public class ProveedorDto
+{
+    public string Nombre { get; set; }
+
+    public string Direccion { get; set; }
+
+    public string Telefono { get; set; }
+
+    public string Correo { get; set; }
+
+}
